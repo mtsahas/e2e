8f28a5beddec8403e598631085ab2fe801867f6f
mtsahas <mtsahas@princeton.edu>
2023-12-10 10:25:35 -0500

    webapp starter

--- webapp/index.js
+++ webapp/index.js
@@ -23,3 +23,9 @@ loginForm.addEventListener("submit", (e) => {
     password.value = "";
   }
 });
+// https://gitlab.matrix.org/matrix-org/olm/-/blob/master/javascript/demo/group_demo.js?ref_type=heads
+// example JS file that creates chat
+// steal
+
+// https://gitlab.matrix.org/matrix-org/olm/-/blob/master/javascript/demo/one_to_one_demo.html?ref_type=heads
+// one to one demo
